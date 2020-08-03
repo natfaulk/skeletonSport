10c6ac49b5348f184c6d3478c7076f3bf03ba141
natfaulk <nfaulk@hotmail.co.uk>
2020-08-03 14:20:27 +1200

    draw person's head

--- src/gameengine/drawPoses.js
+++ src/gameengine/drawPoses.js
@@ -1,7 +1,8 @@
 const logger = require('@natfaulk/supersimplelogger')('Draw poses')
+const Utils = require('./utils')
 
 const color = 'red'
-const lineWidth = 10
+const lineWidth = 20
 
 function drawSkeleton(adjacentKeyPoints, ctx, scale = 1) {
   // const adjacentKeyPoints =
@@ -11,7 +12,47 @@ function drawSkeleton(adjacentKeyPoints, ctx, scale = 1) {
     drawSegment(
         toTuple(keypoints[0].position), toTuple(keypoints[1].position), color,
         scale, ctx);
-  });
+  })
+}
+
+const HEAD_PARTS = [
+  'nose',
+  'leftEye',
+  'rightEye',
+  'leftEar',
+  'rightEar'
+]
+
+let drawHead = (_kps, ctx) => {
+  let headPts = []
+
+  _kps.forEach(_point => {
+    if (HEAD_PARTS.includes(_point.part)) headPts.push(_point.position)
+  })
+
+  if (headPts.length < 2) return
+
+  let centre = Utils.newPt()
+
+  headPts.forEach(_pt => {
+    centre.x += _pt.x
+    centre.y += _pt.y
+  })
+
+  centre.x /= headPts.length
+  centre.y /= headPts.length
+
+  let maxDist = 0
+  headPts.forEach(_pt => {
+    let dist = Utils.dist(centre, _pt)
+    if (dist > maxDist) maxDist = dist
+  })
+  
+  ctx.beginPath()
+  ctx.arc(centre.x, centre.y, maxDist, 0, 2 * Math.PI)
+  ctx.lineWidth = lineWidth
+  ctx.strokeStyle = color
+  ctx.stroke()
 }
 
 /**
@@ -54,6 +95,7 @@ let drawAll = (_canvas, _poses) => {
   _poses.forEach(_pose => {
     drawSkeleton(_pose.adjKps, _canvas.ctx)
     drawKeypoints(_pose.kps, 0.1, _canvas.ctx)
+    drawHead(_pose.kps, _canvas.ctx)
   })
 }
 
